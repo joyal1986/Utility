a423e44de640a9755c450f58f72e9dbc7a891b87
Joyal-151986 <joyjoy@deloitte.nl>
2024-02-27 22:27:55 +0100

    Modify the existing code to be able to format the output in two different ways: 1. As previously: 2 hours 3 minutes 34 seconds 2. Short form: 2h 3m 34s
    Added new version in pom.xml.
    Modify the existing format functions to accomodate different formats.
    Ammended unit tests.

--- src/main/java/com/rabobank/digital/util/time/Format.java
+++ src/main/java/com/rabobank/digital/util/time/Format.java
@@ -3,5 +3,8 @@ package com.rabobank.digital.util.time;
 public enum Format {
     seconds,
     minutes_seconds,
-    hours_minutes_seconds
+    hours_minutes_seconds,
+    h_m_s,
+    m_s,
+    s
 }
--- src/test/java/com/rabobank/digital/util/time/TimeUtilTest.java
+++ src/test/java/com/rabobank/digital/util/time/TimeUtilTest.java
@@ -30,6 +30,11 @@ public class TimeUtilTest {
         assertEquals("45 seconds", TimeUtil.format(45000,Unit.MILLISECONDS,Format.minutes_seconds));
         assertEquals("3 minutes 34 seconds", TimeUtil.format(214000,Unit.MILLISECONDS,Format.minutes_seconds));
         assertEquals("0 seconds", TimeUtil.format(100,Unit.MILLISECONDS,Format.minutes_seconds));
+
+        assertEquals("2m", TimeUtil.format(120000,Unit.MILLISECONDS, Format.m_s));
+        assertEquals("45s", TimeUtil.format(45000,Unit.MILLISECONDS,Format.m_s));
+        assertEquals("3m 34s", TimeUtil.format(214000,Unit.MILLISECONDS,Format.m_s));
+        assertEquals("0s", TimeUtil.format(100,Unit.MILLISECONDS,Format.m_s));
     }
     @Test
     public void testFormatHoursToHoursMinutesSeconds() {
@@ -37,5 +42,10 @@ public class TimeUtilTest {
         assertEquals("5 hours 3 minutes 14 seconds", TimeUtil.format(18194000,Unit.MILLISECONDS,Format.hours_minutes_seconds));
         assertEquals("4 seconds", TimeUtil.format(4000,Unit.MILLISECONDS,Format.hours_minutes_seconds));
         assertEquals("0 seconds", TimeUtil.format(40,Unit.MILLISECONDS,Format.hours_minutes_seconds));
+
+        assertEquals("3m 34s", TimeUtil.format(214000,Unit.MILLISECONDS,Format.h_m_s));
+        assertEquals("5h 3m 14s", TimeUtil.format(18194000,Unit.MILLISECONDS,Format.h_m_s));
+        assertEquals("4s", TimeUtil.format(4000,Unit.MILLISECONDS,Format.h_m_s));
+        assertEquals("0s", TimeUtil.format(40,Unit.MILLISECONDS,Format.h_m_s));
     }
 }
