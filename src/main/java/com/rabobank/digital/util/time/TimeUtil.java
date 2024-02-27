5194de65475f2643dded0a1d6ab71acc78a1e455
Joyal-151986 <joyjoy@deloitte.nl>
2024-02-27 18:38:04 +0100

    Code change for Task-1 which basically require a function which take milliseconds as input and convert milliseconds to  seconds in a specific format.Eg. 5000 milliseconds -> 5 seconds
    Added new version in pom.xml.
    Added dependency require for unit tests in pom.xml
    Created the new function in TimeUtil class.
    Added unit tests.
    Added a final keyword to  TimeUtil class to make it immutable.

--- src/main/java/com/rabobank/digital/util/time/TimeUtil.java
+++ src/main/java/com/rabobank/digital/util/time/TimeUtil.java
@@ -1,4 +1,11 @@
 package com.rabobank.digital.util.time;
-public class TimeUtil {
-    private TimeUtil() {}
+
+public final class TimeUtil {
+    private TimeUtil() {
+    }
+    public static String formatMillisecondsToSeconds(long milliseconds) {
+        long seconds = milliseconds / 1000;
+        return seconds + " seconds";
+    }
+
 }
--- src/test/java/com/rabobank/digital/util/time/TimeUtilTest.java
+++ src/test/java/com/rabobank/digital/util/time/TimeUtilTest.java
@@ -1,6 +1,13 @@
 package com.rabobank.digital.util.time;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TimeUtilTest {
-
+    @Test
+    public void testFormatMillisecondsToSeconds() {
+        assertEquals("5 seconds", TimeUtil.formatMillisecondsToSeconds(5000));
+        assertEquals("0 seconds", TimeUtil.formatMillisecondsToSeconds(0));
+    }
 }
