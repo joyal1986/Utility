b61fe59f13edd25b0eb99116f98e36006a9591b9
Joyal-151986 <joyjoy@deloitte.nl>
2024-02-27 19:26:13 +0100

    Code change for Task-1 which basically require a function which take milliseconds as input and convert milliseconds to  seconds in a specific format.Eg. 5000 milliseconds -> 5 seconds
    Added new version in pom.xml.
    Added dependency require for unit tests in pom.xml
    Created the new function in TimeUtil class.
    Added unit tests.
    Added a final keyword to  TimeUtil class to make it immutable.

--- src/test/java/com/rabobank/digital/util/time/TimeUtilTest.java
+++ src/test/java/com/rabobank/digital/util/time/TimeUtilTest.java
@@ -3,12 +3,25 @@ package com.rabobank.digital.util.time;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class TimeUtilTest {
     @Test
     public void testFormatMillisecondsToSeconds() {
-        assertEquals("5 seconds", TimeUtil.formatMillisecondsToSeconds(5000));
-        assertEquals("10 seconds", TimeUtil.formatMillisecondsToSeconds(10145));
-        assertEquals("0 seconds", TimeUtil.formatMillisecondsToSeconds(10));
+        assertEquals("5 seconds", TimeUtil.format(5000,Unit.MILLISECONDS,Format.seconds));
+
+        assertEquals("10 seconds", TimeUtil.format(10145,Unit.MILLISECONDS,Format.seconds));
+
+        assertEquals("0 seconds", TimeUtil.format(10,Unit.MILLISECONDS,Format.seconds));
+
+        NullPointerException timeFormatNullException = assertThrows(NullPointerException.class, () -> {
+            TimeUtil.format(10,Unit.MILLISECONDS,null);
+        });
+        assertEquals("Time Format can not be null", timeFormatNullException.getMessage());
+
+        NullPointerException timeUnitNullException = assertThrows(NullPointerException.class, () -> {
+            TimeUtil.format(10,null,Format.seconds);
+        });
+        assertEquals("Time Unit can not be null", timeUnitNullException.getMessage());
     }
 }
